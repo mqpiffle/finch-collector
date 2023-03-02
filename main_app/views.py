f06ec4f21f724a5f5131b8d8f642d87c2d26c2cd
Eric Elsner <ericelsnercodes@gmail.com>
2023-03-02 15:21:03 -0500

    inital commit

---
+++ main_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+finches = [
+    {'name': 'Tweetz', 'tax_subclass': 'Mycerobas', 'age_days': 210},
+    {'name': 'Scooter', 'tax_subclass': 'Procardeulis', 'age_days': 55}
+]
+# Create your views here.
